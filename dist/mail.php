fef612285deeba2c43088f3a634008fdd166743d
Lia5 <Liaforweb@gmail.com>
2018-07-29 21:42:01 +0300

    php mail

--- dist/mail.php
+++ dist/mail.php
@@ -12,12 +12,14 @@ if ( $method === 'POST' ) {
 
 	foreach ( $_POST as $key => $value ) {
 		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
-			$message .= "
-			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-			</tr>
-			";
+			foreach ( $_POST as $key => $alt ) {
+				$message .= "
+				" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+					<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+					<td style='padding: 10px; border: #e9e9e9 1px solid;'>$alt</td>
+				</tr>
+				";
+			}
 		}
 	}
 } else if ( $method === 'GET' ) {
@@ -28,12 +30,14 @@ if ( $method === 'POST' ) {
 
 	foreach ( $_GET as $key => $value ) {
 		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
-			$message .= "
-			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-			</tr>
-			";
+			foreach ( $_POST as $key => $alt ) {
+				$message .= "
+				" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+					<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+					<td style='padding: 10px; border: #e9e9e9 1px solid;'>$alt</td>
+				</tr>
+				";
+			}
 		}
 	}
 }
